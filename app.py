52557a73b36230a8e6d9854b99e92ec4f3b4e9ca
Jerlin007 <jerlin.jzy7@gmail.com>
2025-08-31 12:48:08 +0530

    app.py

--- app.py
+++ app.py
@@ -178,6 +178,4 @@ def upload():
 # -----------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))  # default 5000 for local, Koyeb sets PORT
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8080)
